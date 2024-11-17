2fdb1fa3e6e0b4bfa16980a102fd1c7f50c702bd
xsefx1925 <xsefx1925@gmail.ru>
2024-11-17 10:35:46 +0300

    ListBox DONE

--- WnApi1/ListBox2/resource.h
+++ WnApi1/ListBox2/resource.h
@@ -4,17 +4,15 @@
 //
 #define IDD_DIALOG1                     101
 #define IDI_ICON1                       103
-#define IDC_STATIC1                     1001
-#define IDC_EDIT_LOGIN                  1002
-#define IDC_EDIT_PASSWORD               1003
+#define IDC_LIST                        1001
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
