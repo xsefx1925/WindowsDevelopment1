2f0d4169d67144821d346e314ff67bdbf518fc28
xsefx1925 <xsefx1925@gmail.ru>
2024-12-02 14:36:20 +0300

    Create Window DONE1

--- WnApi1/Calc/main.cpp
+++ WnApi1/Calc/main.cpp
@@ -1,6 +1,39 @@
 #include<Windows.h>
+#define IDC_EDIT_DISPLAY  999
+#define IDC_BUTTON_0      1000
+#define IDC_BUTTON_1      1001
+#define IDC_BUTTON_2      1002
+#define IDC_BUTTON_3      1003
+#define IDC_BUTTON_4      1004
+#define IDC_BUTTON_5      1005
+#define IDC_BUTTON_6      1006
+#define IDC_BUTTON_7      1007
+#define IDC_BUTTON_8      1008
+#define IDC_BUTTON_9      1009
+#define IDC_BUTTON_POINT  1010
+
+#define IDC_BUTTON_PLUS   1011
+#define IDC_BUTTON_MINUS  1012
+#define IDC_BUTTON_ASTER  1013 //"*"
+#define IDC_BUTTON_SLASH  1014//  /
+
+#define IDC_BUTTON_BSP    1015 //Backspase
+#define IDC_BUTTON_SLR    1016 //clear
+#define IDC_BUTTON_EQUAL  1017 //"="
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "Calc_WPD_311";
+
+CONST CHAR g_i_SCREEN_WIDTH = 400;
+CONST CHAR g_i_SCREEN_HEIGHT = 22;
+
+
+CONST INT g_i_BUTTON_SIZE = 50; //размер кнопки в пикселях
+CONST INT g_i_INTERVAL = 5; //расстояние между кнопками
+CONST INT g_i_START_X = 10;
+CONST INT g_i_START_Y = 10;
+CONST INT g_i_BUTTON_START_X = g_i_START_X;
+CONST INT g_i_BUTTON_START_Y = g_i_START_Y+ g_i_SCREEN_HEIGHT+g_i_INTERVAL;
+
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -62,6 +95,20 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch(uMsg)
 	{
 	case WM_CREATE:
+	{
+		HWND hEdit = CreateWindowEx
+		(
+			NULL, "Edit", "0",
+			WS_CHILD | WS_VISIBLE | WS_BORDER|ES_RIGHT,
+			10, 10,
+			400, 22,
+			hwnd,
+			(HMENU)1000,
+			GetModuleHandle(NULL),
+			NULL
+		);
+	}
+		
 		break;
 	case WM_COMMAND:
 		break;
