2733c3058b19c6280d9674fea57c67030ecfc0db
xsefx1925 <xsefx1925@gmail.ru>
2024-12-08 12:40:06 +0300

    Bugs fixed

--- WnApi1/Calc/main.cpp
+++ WnApi1/Calc/main.cpp
@@ -52,7 +52,7 @@ CONST INT g_i_WINDOW_HEIGHT = g_i_START_X + g_i_SCREEN_HEIGHT + g_i_BUTTON_SIZE 
 */
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "Calc_WPD_311";
-CONST CHAR* g_OPERATIONS[] = { "+","-", "*",  "/"  };
+CONST CHAR* g_OPERATIONS[] = { "+","-", "*",  "/" };
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 INT GetTitleBarHeight(HWND hwnd);
@@ -118,10 +118,10 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 	switch (uMsg)
 	{
-	    case WM_CREATE:
-	  {
+	case WM_CREATE:
+	{
 		HWND hEdit = CreateWindowEx
-		   (
+		(
 			NULL, "Edit", "0",
 			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_RIGHT,
 			10, 10,
@@ -130,31 +130,31 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			(HMENU)999,
 			GetModuleHandle(NULL),
 			NULL
-		   );
+		);
 		CHAR sz_digit[2] = {};
-		  for (int i = 6; i >= 0; i -= 3)
-		  {
-			  for (int j = 0; j < 3; j++)
-			  {
+		for (int i = 6; i >= 0; i -= 3)
+		{
+			for (int j = 0; j < 3; j++)
+			{
 				sz_digit[0] = i + j + '1';
 				CreateWindowEx
-				   (
+				(
 					NULL, "Button", sz_digit,
 					WS_CHILD | WS_VISIBLE,
 					g_i_BUTTON_START_X + (g_i_BUTTON_SIZE + g_i_INTERVAL) * j,
-					g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) * (i / 3),
+					g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) * (2 - i / 3),
 					g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
 					hwnd,
-					(HMENU)IDC_BUTTON_0 + (i + j),
+					(HMENU)(IDC_BUTTON_1 + i + j),
 					GetModuleHandle(NULL),
 					NULL
-				   );
+				);
 
-			  }
+			}
 
-		  }
+		}
 		CreateWindowEx
-		   (
+		(
 			NULL, "Button", "0",
 			WS_CHILD | WS_VISIBLE,
 			BUTTON_SHIFT_X(0), BUTTON_SHIFT_Y(3),
@@ -164,9 +164,9 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			(HMENU)IDC_BUTTON_0,
 			GetModuleHandle(NULL),
 			NULL
-		   );
+		);
 		CreateWindowEx
-		   (
+		(
 			NULL, "Button", ".",
 			WS_CHILD | WS_VISIBLE,
 			BUTTON_SHIFT_X(2), BUTTON_SHIFT_Y(3),
@@ -177,11 +177,11 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			(HMENU)IDC_BUTTON_POINT,
 			GetModuleHandle(NULL),
 			NULL
-		   );
-		  for (int i = 0; i < 4; i++)
-		  {
+		);
+		for (int i = 0; i < 4; i++)
+		{
 			CreateWindowEx
-			   (
+			(
 				NULL, "Button", g_OPERATIONS[i],
 				WS_CHILD | WS_VISIBLE,
 				BUTTON_SHIFT_X(3), BUTTON_SHIFT_Y(3 - i),
@@ -192,84 +192,84 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				(HMENU)IDC_BUTTON_PLUS + i,
 				GetModuleHandle(NULL),
 				NULL
-			   );
-		  }
-		      CreateWindowEx
-		     (
-			   NULL, "Button", "<-",
-			   WS_CHILD | WS_VISIBLE,
-			   BUTTON_SHIFT_X(4), BUTTON_SHIFT_Y(0),
-			   g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
-			   hwnd,
-			   (HMENU)IDC_BUTTON_BSP,
-			   GetModuleHandle(NULL),
-			   NULL
-		      );
-	  
-	           CreateWindowEx
-	          (
-		       NULL, "Button", "C",
-		       WS_CHILD | WS_VISIBLE,
-		       BUTTON_SHIFT_X(4), BUTTON_SHIFT_Y(1),
-		       g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
-		       hwnd,
-		       (HMENU)IDC_BUTTON_CLR,
-		       GetModuleHandle(NULL),
-		       NULL
-	          );
-	      CreateWindowEx
-	       (
-		     NULL, "Button", "=",
-		     WS_CHILD | WS_VISIBLE,
-		     BUTTON_SHIFT_X(4), BUTTON_SHIFT_Y(2),
-		     g_i_BUTTON_SIZE, g_i_BUTTON_DOUBLE_SIZE,
-		     hwnd,
-		     (HMENU)IDC_BUTTON_EQUAL,
-		     GetModuleHandle(NULL),
-		     NULL
-	       );
-	         INT title_bar_height = GetTitleBarHeight(hwnd);
-	  }
-       break;
+			);
+		}
+		CreateWindowEx
+		(
+			NULL, "Button", "<-",
+			WS_CHILD | WS_VISIBLE,
+			BUTTON_SHIFT_X(4), BUTTON_SHIFT_Y(0),
+			g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_BSP,
+			GetModuleHandle(NULL),
+			NULL
+		);
 
-	   case WM_COMMAND:
-	   {
-		   HWND hEditDisplay = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
-		   CONST INT SIZE = 256;
-		    CHAR sz_display[MAX_PATH]{};
-			CHAR  sz_digit[2]{};
-		   if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
-		   {
-			   sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
-			   SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
-			   if (strlen(sz_display) == 1 && sz_display[0] == '0')
-			   {
-				   sz_display[0] = sz_digit[0];
-			   }
-				  
-			   else
-			   {
-				   strcat(sz_display, sz_digit);
-			   }
-				   
-			   SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
-		   }
-		   if (LOWORD(wParam) == IDC_BUTTON_POINT)
-		   {
-			   SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
-			   if (strchr(sz_display, '.'))break;
-			   strcat(sz_display, ".");
-			   SendMessage(hEditDisplay, WM_GETTEXT, 0, (LPARAM)sz_display);
-		   }
-	   }
-		   break;
-	   case WM_DESTROY:
+		CreateWindowEx
+		(
+			NULL, "Button", "C",
+			WS_CHILD | WS_VISIBLE,
+			BUTTON_SHIFT_X(4), BUTTON_SHIFT_Y(1),
+			g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_CLR,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "Button", "=",
+			WS_CHILD | WS_VISIBLE,
+			BUTTON_SHIFT_X(4), BUTTON_SHIFT_Y(2),
+			g_i_BUTTON_SIZE, g_i_BUTTON_DOUBLE_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_EQUAL,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		INT title_bar_height = GetTitleBarHeight(hwnd);
+	}
+	break;
+
+	case WM_COMMAND:
+	{
+		HWND hEditDisplay = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+		CONST INT SIZE = 256;
+		CHAR sz_display[MAX_PATH]{};
+		CHAR  sz_digit[2]{};
+		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
+		{
+			sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
+			SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+			if (strlen(sz_display) == 1 && sz_display[0] == '0')
+			{
+				sz_display[0] = sz_digit[0];
+			}
+
+			else
+			{
+				strcat(sz_display, sz_digit);
+			}
+
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_POINT)
+		{
+			SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+			if (strchr(sz_display, '.'))break;
+			strcat(sz_display, ".");
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+		}
+	}
+	break;
+	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
-	   case WM_CLOSE:
+	case WM_CLOSE:
 		DestroyWindow(hwnd);
 		break;
-	   default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return FALSE;
 }
@@ -280,6 +280,6 @@ INT GetTitleBarHeight(HWND hwnd)
 	GetWindowRect(hwnd, &window_rect);
 	GetClientRect(hwnd, &client_rect);
 	INT title_bar_height = (window_rect.bottom - window_rect.top) - (client_rect.bottom - client_rect.top);
-	return title_bar_height; 
+	return title_bar_height;
 
 }
